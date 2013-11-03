f62bd6b48bdba0e9dc32cc1db75dac130de273ba
Nate Rowland <nochte@gmail.com>
2013-11-03 11:50:50 -0600

    fleshed out run_worker

--- bin/run_worker.rb
+++ bin/run_worker.rb
@@ -1,9 +1,12 @@
+#!/usr/bin/env ruby
+
 require "./lib/message/router/version"
 require "./lib/message/worker"
 
 #here, we're going to read a config file to determine
 #  things like what worker class (a symbol) to load
 
-worker = Message::Worker::Worker.new
+worker = Message::Worker::Base.new
 worker.setup
 worker.run
+
